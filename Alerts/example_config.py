cc7a7a9c09c26f73512b55bbc73073240aa74bbd
jaustinj <austinjacobs@gmail.com>
2018-04-06 00:35:57 -0700

    UPDATE: example config file

--- Alerts/example_config.py
+++ Alerts/example_config.py
@@ -1,17 +1,51 @@
-# Config for SixPm services
+# Config for email
 EMAIL_CONFIG = {
-    'SEND_FROM': '', # from email
-    'SEND_TO': [''], # list of email contacts
-    'USERNAME': '', # username login
-    'PASSWORD': '' # password login
+    'SEND_FROM': '',
+    'SEND_TO': [''],
+    'USERNAME': '',
+    'PASSWORD': '',
 }
 
-SIERRA_INGEST_CONFIG = {
+# Scraper config for Sierra Trading Post
+SIERRA_CONFIG = {
     'SEARCHES': [
                  'oakley sunglasses',
                  'smith optics eyewear',
                  'costa eyewear'
                  ],
+    'DELAY_BETWEEN_PAGE_REQUESTS': 5,
+    'POSTGRES_BASE_TABLE': 'sierra',
+    'POSTGRES_ALERT_TABLE': 'sierra_alerted',
+    'POSTGRES_SKU_COLUMN': 'title',
+    'THRESHOLD_COLUMN': 'perc_off',
+    'THRESHOLD_TYPE': '>=',
     'THRESHOLD': '0.60',
-    'DELAY_BETWEEN_PAGE_REQUESTS' = 60
+    'OUTPUT_FILE_NAME': 'sierra_alerts',
+    'SEARCH_EBAY_COLUMN': 'title',
+    'EMAIL_SUBJECT': 'Sierra Deal Alert!'
+
+}
+
+# Scraper config for Backcountry alerts
+BACKCOUNTRY_CONFIG = {
+    'SEARCHES': [
+                 'oakley sunglasses',
+                 'smith optics eyewear',
+                 'costa eyewear'
+                 ],
+    'DELAY_BETWEEN_PAGE_REQUESTS': 5,
+    'POSTGRES_BASE_TABLE': 'backcountry',
+    'POSTGRES_ALERT_TABLE': 'backcountry_alerted',
+    'POSTGRES_SKU_COLUMN': 'url',
+    'THRESHOLD_COLUMN': 'perc_off',
+    'THRESHOLD_TYPE': '>=',
+    'THRESHOLD': '60',
+    'OUTPUT_FILE_NAME': 'backcountry_alerts',
+    'SEARCH_EBAY_COLUMN': 'title',
+    'EMAIL_SUBJECT': 'Backcountry Deal Alert!'
+
+}
+
+ALERT_CONFIG = {
+    'TIME_BETWEEN_CHECKS': 60*15
 }
